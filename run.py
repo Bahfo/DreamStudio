723e4b3986504973c78a5decb273b96034f5cdaa
bahaa_is_safe <bahnofal2005@gmail.com>
2026-02-14 19:55:12 +0300

    Minor Changes on Text Locations and Files 2

--- run.py
+++ run.py
@@ -1,6 +1,8 @@
 from widgets.initializer import App
+from widgets.welcome import WelcomeWindow
 
-# RUN EVERYTHING 
+# CODE REFACTOR FOR MAINTAINING ALL STEPS
 if __name__ == '__main__':
     app = App(None)
+    welcome_window = WelcomeWindow()
     app.run()
