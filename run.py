d633107b01cccc537a64fbdd5e28faa9f2b5b882
bahaa_is_safe <bahnofal2005@gmail.com>
2026-03-05 16:19:57 +0300

    Minor Changes

--- run.py
+++ run.py
@@ -3,9 +3,9 @@ from startup import *
 
 ctk.set_appearance_mode("light")
 
-# CODE REFACTOR FOR MAINTAINING ALL STEPS
 if __name__ == '__main__':
     app = App(None) 
     app.run()
+
     # welcome_window = Initialize()
     # welcome_window.start_loading_screen()
